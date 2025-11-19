17b01bf10d4a68802ec83578514554feca7b5bc9
DmtryFilipenko <102276912+DmtryFil@users.noreply.github.com>
2025-11-19 21:33:01 +0500

    Add files via upload

---
+++ code/15.py
@@ -0,0 +1,7 @@
+from lab15 import square
+
+if __name__ == '__main__':
+    a = int(input("Введите a: "))
+    b = int(input("Введите b: "))
+    c = int(input("Введите c: "))
+    print(square(a, b, c))
